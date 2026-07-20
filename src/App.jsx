5f239d80fc2ecccd0fb08323a4ded79615d99913
Anthony Heang <antrithea12@gmail.com>
2026-07-19 21:20:12 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+function Sidebar() {
+    return (
+        <aside className="sidebar"> 
+
+        </aside>
+    )
+}
+
+export default Sidebar;
+
